7289c648bec8c2438fe82070e6f609ccd86016e0
htakaki <htakaki@oneup-inc.com>
2013-06-04 02:38:08 +0900

    typezero file new add

---
+++ js/common.js
@@ -0,0 +1,39 @@
+$(function(){
+
+  // iphone statusBar Hidden
+  function hideURLbar() { window.scrollTo(0,1); }
+  addEventListener('load', function() { setTimeout(hideURLbar, 100); }, false );
+
+  // global Accordion Menu
+  var menuBtn = $("#menuBtn");
+  var menuBox = $("#menuBox");
+  menuBox.hide();
+  menuBtn.click(function(){
+    menuBox.slideUp();
+    if($(menuBox).css("display")=="none"){
+      $(menuBox).slideDown();
+    }
+  });
+
+  var menuClose = $("#menuClose");
+  menuClose.click(function(){
+    menuBox.slideUp();
+  });
+
+  // touchHover -> touchstart change script when touch btn.
+  $(".touchHover").bind('touchstart',function(){
+    $(this).addClass("touchstart");
+  });
+  $(".touchHover").bind('touchend',function(){
+    $(this).removeClass("touchstart");
+  });
+
+  $(".flexslider").flexslider({
+      animation: "slide",
+      slideshowSpeed: 3000,
+      controlNav: ".slideArrow",
+      prevText: "",
+      nextText: ""
+  });
+});
+
